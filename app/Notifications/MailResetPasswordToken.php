d9ec67e1a5188a85bc4bf3372a3260a150a08e6d
denisgruba <DenisGruba@tha-ictweb05.local>
2017-08-17 11:39:41 +0100

    Edit password reset

--- app/Notifications/MailResetPasswordToken.php
+++ app/Notifications/MailResetPasswordToken.php
@@ -18,7 +18,7 @@ class MailResetPasswordToken extends Notification
      */
     public function __construct()
     {
-        //
+        $this->token = $token;
     }
 
     /**
@@ -41,9 +41,10 @@ class MailResetPasswordToken extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+            ->subject("Reset your password")
+            ->line("You are receiving this email because we received a password reset request for your account.")
+            ->action('Reset Password', url('password/reset', $this->token))
+            ->line('If you did not request a password reset, no further action is required.');
     }
 
     /**
